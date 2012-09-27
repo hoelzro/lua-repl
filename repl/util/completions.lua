6531f67bb53678c58298dd2ba415761908e9cff3
Rob Hoelz <rob@hoelz.ro>
2012-09-27 21:34:13 +0200

    Add completions utility library

---
+++ repl/util/completions.lua
@@ -0,0 +1,127 @@
+-- Copyright (c) 2011-2012 Rob Hoelz <rob@hoelz.ro>
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of
+-- this software and associated documentation files (the "Software"), to deal in
+-- the Software without restriction, including without limitation the rights to
+-- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+-- the Software, and to permit persons to whom the Software is furnished to do so,
+-- subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+-- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+-- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+-- @class repl.util.completions
+--  This module provides completion facilities.
+
+-- XXX this functionality should be provided by a plugin
+local getmetatable = getmetatable
+local pairs        = pairs
+local sfind        = string.find
+local sgmatch      = string.gmatch
+local ssub         = string.sub
+local tconcat      = table.concat
+local type         = type
+
+local function isindexable(value)
+  if type(value) == 'table' then
+    return true
+  end
+
+  local mt = getmetatable(value)
+
+  return mt and mt.__index
+end
+
+local function getcompletions(t)
+  local union = {}
+
+  while isindexable(t) do
+    if type(t) == 'table' then
+      -- XXX what about the pairs metamethod in 5.2?
+      --     either we don't care, we implement a __pairs-friendly
+      --     pairs for 5.1, or implement a 'rawpairs' for 5.2
+      for k, v in pairs(t) do
+        if union[k] == nil then
+          union[k] = v
+        end
+      end
+    end
+
+    local mt = getmetatable(t)
+    t        = mt and mt.__index or nil
+  end
+
+  return pairs(union)
+end
+
+local function split_ns(expr)
+  if expr == '' then
+    return { '' }
+  end
+
+  local pieces = {}
+
+  -- XXX method calls too (option?)
+  for m in sgmatch(expr, '[^.]+') do
+    pieces[#pieces + 1] = m
+  end
+
+  -- logic for determining whether to pad the matches with the empty
+  -- string (ugly)
+  if ssub(expr, -1) == '.' then
+    pieces[#pieces + 1] = ''
+  end
+
+  return pieces
+end
+
+local function determine_ns(expr)
+  local ns     = _G -- XXX what if the REPL lives in a special context? (option?)
+  local pieces = split_ns(expr)
+
+  for index = 1, #pieces - 1 do
+    local key = pieces[index]
+    -- XXX rawget? or regular access? (option?)
+    ns = ns[key]
+  end
+
+  expr = pieces[#pieces]
+
+  local prefix = ''
+
+  if #pieces > 1 then
+    prefix = tconcat(pieces, '.', 1, #pieces - 1) .. '.'
+  end
+
+  return ns, prefix, expr
+end
+
+local function complete(expr, callback)
+  local ns, prefix
+
+  ns, prefix, expr = determine_ns(expr)
+
+  for k, v in getcompletions(ns) do
+    if sfind(k, expr, 1, true) == 1 then
+      local suffix = ''
+      local type   = type(v)
+
+      -- XXX this should be optional
+      if type == 'function' then
+        suffix = '('
+      elseif type == 'table' then
+        suffix = '.'
+      end
+
+      callback(prefix .. k .. suffix)
+    end
+  end
+end
+
+return complete
