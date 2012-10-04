7acefa1e9d9d3ce38fd80f4eaafe1aea8570594c
Rob Hoelz <rob@hoelz.ro>
2012-10-04 20:46:36 +0200

    Make sure features are propagated among clones

--- t/plugin-feature-tests.lua
+++ t/plugin-feature-tests.lua
@@ -3,7 +3,7 @@ local utils = require 'test-utils'
 pcall(require, 'luarocks.loader')
 require 'Test.More'
 
-plan(8)
+plan(14)
 
 do -- basic tests {{{
   local clone = repl:clone()
@@ -60,4 +60,26 @@ do -- conflicts {{{
   like(err, tostring(line_no) .. ': feature "foo" already present')
 
   -- XXX what about methods injected into the object?
+end -- }}}
+
+do -- clone:hasfeature {{{
+  local child = repl:clone()
+
+  child:loadplugin(function()
+    features = 'foo'
+  end)
+
+  local grandchild = child:clone()
+
+  ok(not repl:hasfeature 'foo')
+  ok(child:hasfeature 'foo')
+  ok(grandchild:hasfeature 'foo')
+
+  child:loadplugin(function()
+    features = 'bar'
+  end)
+
+  ok(not repl:hasfeature 'bar')
+  ok(child:hasfeature 'bar')
+  ok(not grandchild:hasfeature 'bar')
 end -- }}}
