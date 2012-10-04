674b38cc5544f09c9cc12500a1af0a11975dac5b
Rob Hoelz <rob@hoelz.ro>
2012-10-04 19:52:05 +0200

    Check what happens when we load a plugin twice

--- t/plugin-basic-tests.lua
+++ t/plugin-basic-tests.lua
@@ -1,8 +1,9 @@
-local repl = require 'repl'
+local repl  = require 'repl'
+local utils = require 'test-utils'
 pcall(require, 'luarocks.loader')
 require 'Test.More'
 
-plan(2)
+plan(4)
 
 local clone = repl:clone()
 
@@ -20,4 +21,28 @@ do -- basic tests {{{
       error 'uh-oh'
     end)
   end, 'uh%-oh')
+
+end -- }}}
+
+do -- loading the same plugin twice {{{
+  local function plugin()
+  end
+
+  local line_no
+
+  clone:loadplugin(plugin)
+  local _, err = pcall(function()
+    line_no = utils.next_line_number()
+    clone:loadplugin(plugin)
+  end)
+  like(err, tostring(line_no) .. ': plugin "function:%s+%S+" has already been loaded')
+
+  _, err = pcall(function()
+    line_no = utils.next_line_number()
+    clone:clone():loadplugin(plugin)
+  end)
+  like(err, tostring(line_no) .. ': plugin "function:%s+%S+" has already been loaded')
+
+  repl:clone():loadplugin(plugin)
+  repl:clone():loadplugin(plugin)
 end -- }}}
