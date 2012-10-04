9fe6df7c19e0bf62954e7bf34bfd22ffd429542f
Rob Hoelz <rob@hoelz.ro>
2012-10-04 20:30:35 +0200

    Add tests for requirefeature

--- t/plugin-feature-tests.lua
+++ t/plugin-feature-tests.lua
@@ -1,8 +1,9 @@
-local repl = require 'repl'
+local repl  = require 'repl'
+local utils = require 'test-utils'
 pcall(require, 'luarocks.loader')
 require 'Test.More'
 
-plan(6)
+plan(7)
 
 do -- basic tests {{{
   local clone = repl:clone()
@@ -22,4 +23,22 @@ do -- basic tests {{{
   ok(clone:hasfeature 'foo')
   ok(clone:hasfeature 'bar')
   ok(clone:hasfeature 'baz')
+end -- }}}
+
+do -- requirefeature {{{
+  local clone = repl:clone()
+
+  clone:loadplugin(function()
+    features = 'foo'
+  end)
+
+  clone:requirefeature 'foo'
+
+  local line_no
+  local _, err = pcall(function()
+    line_no = utils.next_line_number()
+    clone:requirefeature 'bar'
+  end)
+
+  like(err, tostring(line_no) .. ': required feature "bar" not present')
 end -- }}}
