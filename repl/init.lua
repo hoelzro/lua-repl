6518a372ae349a0759768945b2f9298feb3567f1
Rob Hoelz <rob@hoelz.ro>
2012-10-20 12:07:36 +0200

    Change default prompt to look like standalone Lua interpreter

--- repl/init.lua
+++ repl/init.lua
@@ -49,7 +49,7 @@ end
 --- Returns the prompt for a given level.
 -- @param level The prompt level. Either 1 or 2.
 function repl:getprompt(level)
-  return level == 1 and 'lua>' or 'lua>>'
+  return level == 1 and '>' or '>>'
 end
 
 --- Displays a prompt for the given prompt level.
