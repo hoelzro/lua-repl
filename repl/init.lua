1c7de3e16fd3ecb9a856e30d446cd799ebf6ed96
Rob Hoelz <rob@hoelz.ro>
2014-10-17 23:11:23 -0500

    Move setfenv fallback into a utils module

--- repl/init.lua
+++ repl/init.lua
@@ -29,25 +29,7 @@ local setmetatable = setmetatable
 local sformat      = string.format
 local smatch       = string.match
 local error        = error
-
-local setfenv = setfenv or function(f, t)
-  local upvalue_index = 1
-
-  -- XXX we may need a utility library if debug isn't available
-  while true do
-    local name = debug.getupvalue(f, upvalue_index)
-    -- some functions don't have an _ENV upvalue, because
-    -- they never refer to globals
-    if not name then
-      return
-    end
-    if name == '_ENV' then
-      debug.setupvalue(f, upvalue_index, t)
-      return
-    end
-    upvalue_index = upvalue_index + 1
-  end
-end
+local setfenv      = require('repl.utils').setfenv
 
 local function gather_results(success, ...)
   local n = select('#', ...)
