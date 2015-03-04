5da015a844d675ba9a8934ef53ef5b2c27097393
Henry Kielmann <henrykielmann@gmail.com>
2015-03-04 23:42:44 +0100

    Finished naive filename completion.

--- repl/plugins/filename_completion.lua
+++ repl/plugins/filename_completion.lua
@@ -1,44 +1,40 @@
 local lfs = require 'lfs'
 assert(lfs, 'LuaFileSystem is missing.')
 
---features = 'completion'
---[[
-Completion suggestions should only span the part of the line which is actually completed.
-
-While the current implementation is good enough for linenoise completion,
-imagine if some
-
-This is no a problem per se, but it opens up the possibility to yield weird
-completion results.  Though you can't display the suggestion in a popup, so the
-user sees all available suggestions at a glance.
- ]]
-
 repl:requirefeature 'completion'
 
 local function guess_directory_separator(file_name)
   return file_name:match('/') or
-         file_name:match('\\\\') or
+         file_name:match('\\') or
          '/'
 end
 
 local function split_parent_directory(file_name)
-  local parent_directory, direcotry_entry file_name:match('^(.+)(\\-[\\/].+)$')
+  local parent_directory, directory_entry =
+    file_name:match('^(.+)[\\/](.+)$')
   if not parent_directory then
     parent_directory = '.'
-    direcotry_entry = file_name
+    directory_entry = file_name
   end
-  return parent_directory, direcotry_entry
+  return parent_directory, directory_entry
 end
 
 local function is_ignored_directory_entry(entry)
-  return entry:find('^[\\/]%.$')
+  return entry == '.'
+end
+
+local function replace_end_of_string(str, suffix, replacement)
+  assert(str:sub(-#suffix) == suffix)
+  return str:sub(1, -(#suffix+1)) .. replacement
 end
 
 local function complete_file_name(file_name, expr, callback)
-  local directory, entry = split_parent_directory(file_name)
+  local directory, partial_entry = split_parent_directory(file_name)
+  print('\n', partial_entry)
   for entry in lfs.dir(directory) do
-    if not is_ignored_directory_entry(entry) then
-      callback(expr..entry)
+    if not is_ignored_directory_entry(entry) and
+       entry:find(partial_entry, 1, true) == 1 then
+      callback(replace_end_of_string(expr, partial_entry, entry))
     end
   end
 end
@@ -56,11 +52,10 @@ function after:complete(expr, callback)
   if str then
     local file_name = expr:match('[%w@/\\.-_+#$%%{}[%]!~ ]+$')
     if file_name then
-      local file_mode = lfs.attributes(file_name, 'mode')
-      if not file_mode then
-        complete_file_name(file_name, expr, callback)
-      elseif file_mode == 'directory' then -- TODO: what about directory links?
+      if file_name:find('[/\\]$') then
         complete_directory(file_name, expr, callback)
+      else
+        complete_file_name(file_name, expr, callback)
       end
     end
   end
